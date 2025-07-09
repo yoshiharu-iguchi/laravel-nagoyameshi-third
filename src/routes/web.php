9294201da9903490fbd870c3e152bb4afded6135
iguigu <iguchi2203@gmail.com>
2025-07-10 08:15:00 +0900

    14章 ファクトリ作成済み

--- src/routes/web.php
+++ src/routes/web.php
@@ -30,4 +30,8 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin
 
     Route::resource('categories',Admin\CategoryController::class)->only(['index','store','update','destroy']);
 
+    Route::resource('company',Admin\CompanyController::class)->only(['index','edit','update']);
+
+    Route::resource('terms',Admin\TermController::class)->only(['index','edit','update']);
+
 });
